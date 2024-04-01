9b1fce5219c281fa522812e48384adcecb05dd28
Abashjinn <a.trudel.photo@gmail.com>
2024-04-01 13:14:57 -0400

    Update ESP32-AdafruitOLED-ntfy_POST.ino
    library cleanup

--- ESP32-AdafruitOLED-ntfy_POST.ino
+++ ESP32-AdafruitOLED-ntfy_POST.ino
@@ -2,7 +2,6 @@
 #include <Wire.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
-#include <ArduinoJson.h>
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
