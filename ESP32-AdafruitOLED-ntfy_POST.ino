3af54b40742af6f89f96bf3cae7a5f92261cff4d
Abashjinn <a.trudel.photo@gmail.com>
2024-04-01 12:56:47 -0400

    Create ESP32-AdafruitOLED-ntfy_POST.ino
    Uses an ESP32-C3  (PIN modifications might be required if you uses another device) with WiFi to POST on ntfy.sh using POST. Just edit stuff like "ntfyToken" and "ssid"/"password" of your own WiFi and it should be good to go or try it on Wokwi. Feel free to fork or send me comments I like to share although I may not respond immediately. Cheers

---
+++ ESP32-AdafruitOLED-ntfy_POST.ino
@@ -0,0 +1,66 @@
+// Built mostly by hand and using Bitcoin ESP32 Bitcoin Price Tracker on Wokwi https://wokwi.com/
+#include <Adafruit_SSD1306.h>
+#include <Wire.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+const char* ntfyServer = "ntfy.sh";
+const int ntfyPort = 80;
+const char* ntfyToken = "Esp32WokwiTest";
+
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+
+void setup() {
+  Serial.begin(115200);
+
+  // Connect to Wi-Fi
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Send push notification
+  sendNotification("Hello from ESP32");
+}
+
+void loop() {
+  // Nothing to do here
+}
+
+void sendNotification(String message) {
+  WiFiClient client;
+  
+  if (!client.connect(ntfyServer, ntfyPort)) {
+    Serial.println("Failed to connect to ntfy.sh");
+    return;
+  }
+
+  String payload = "{\"action\": \"push\", \"title\": \"ESP32\", \"message\": \"" + message + "\"}";
+
+  client.print(String("POST /Esp32WokwiTest HTTP/1.1\r\n") +
+               "Host: " + ntfyServer + "\r\n" +
+               "Actions: http, Push notification, http://" + ntfyServer + ntfyToken + ", method=POST, body=" + payload + "\r\n" +
+               "Connection: close\r\n\r\n");
+
+  delay(500); // Wait for server response
+
+  while (client.available()) {
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  Serial.println();
+  Serial.println("Notification sent");
+  client.stop();
+}
